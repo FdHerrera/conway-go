c0d56ca2dc2a97ede764135df757b19fdd263850
Federico Herrera <federico.herrera_dev@outlook.com>
2023-12-04 16:32:14 -0300

    Add calc next gen func, add test

--- main.go
+++ main.go
@@ -33,6 +33,45 @@ func initGrid() grid {
 	return grid
 }
 
+func calcNextGen(grid grid) grid {
+	gridCopy := make([][]int, len(grid))
+	for i, col := range grid {
+		colCopy := make([]int, len(col))
+		copy(colCopy, col)
+		gridCopy[i] = colCopy
+	}
+	applyToEach(
+		func(grid [][]int, row, col int) {
+			neighboursCount := 0
+			for i := -1; i < 2; i++ {
+				for j := -1; j < 2; j++ {
+					if i == 0 && j == 0 {
+						continue
+					}
+					if row+i < 0 || col+j < 0 {
+						continue
+					}
+					if row+i == len(grid) || col+j == len(grid[0]) {
+						continue
+					}
+					neighboursCount += grid[row+i][col+j]
+				}
+			}
+			if neighboursCount < 2 {
+				gridCopy[row][col] = 0
+			}
+			if neighboursCount > 3 {
+				gridCopy[row][col] = 0
+			}
+			if grid[row][col] == 0 && neighboursCount == 3 {
+				gridCopy[row][col] = 1
+			}
+		},
+		grid,
+	)
+	return gridCopy
+}
+
 func applyToEach(funcToApply func(grid [][]int, row int, col int), grid grid) {
 	for row := 0; row < len(grid); row++ {
 		for col := 0; col < len(grid[row]); col++ {
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -37,4 +38,68 @@ func Test_initGrid(t *testing.T) {
 	if colsWithTenRows {
 		t.Error("Each col should have 10 rows")
 	}
+}
+
+func Test_calcNextGen(t *testing.T) {
+	cases := []struct {
+		initial  [][]int
+		expected [][]int
+	}{
+		{
+			[][]int{
+				{0, 1, 0},
+				{1, 0, 0},
+				{0, 1, 0},
+			}, [][]int{
+				{0, 0, 0},
+				{1, 1, 0},
+				{0, 0, 0},
+			},
+		},
+		{
+			[][]int{
+				{0, 1, 0},
+				{0, 1, 0},
+				{0, 1, 0},
+			},
+			[][]int{
+				{0, 0, 0},
+				{1, 1, 1},
+				{0, 0, 0},
+			},
+		},
+		{
+			[][]int{
+				{0, 0, 0, 0},
+				{0, 1, 1, 0},
+				{0, 1, 1, 0},
+				{0, 0, 0, 0},
+			},
+			[][]int{
+				{0, 0, 0, 0},
+				{0, 1, 1, 0},
+				{0, 1, 1, 0},
+				{0, 0, 0, 0},
+			},
+		},
+	}
+
+	for i, e := range cases {
+		nextGen := calcNextGen(e.initial)
+		if !areEquals(nextGen, e.expected) {
+			t.Errorf("Case [%d] failed, not equals\n %d \n %d", i, nextGen, e.expected)
+		}
+	}
+}
+
+func areEquals(actual, expected [][]int) bool {
+	if len(actual) != len(expected) {
+		return false
+	}
+	for i := 0; i < len(actual); i++ {
+		if !reflect.DeepEqual(actual[i], expected[i]) {
+			return false
+		}
+	}
+	return true
 }
